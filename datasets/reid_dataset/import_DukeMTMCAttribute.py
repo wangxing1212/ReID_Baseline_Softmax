8ccd85076f11ada06209e0bad3c00d53bb9715a8
Unknown <LinShanify@gmail.com>
2018-03-12 00:58:11 +0800

    fix bugs in attribute labeling

--- datasets/reid_dataset/import_DukeMTMCAttribute.py
+++ datasets/reid_dataset/import_DukeMTMCAttribute.py
@@ -8,29 +8,53 @@ def import_DukeMTMCAttribute(dataset_dir):
     train,query,test = import_DukeMTMC(dataset_dir)
     if not os.path.exists(os.path.join(dataset_dir,dataset_name)):
         print('Please Download the DukeMTMCATTributes Dataset')
-    label=['backpack',
-           'bag',
-           'handbag',
-           'boots',
-           'gender',
-           'hat',
-           'shoes',
-           'top',
-           'downblack',
-           'downwhite',
-           'downred',
-           'downgray',
-           'downblue',
-           'downgreen',
-           'downbrown',
-           'upblack',
-           'upwhite',
-           'upred',
-           'uppurple',
-           'upgray',
-           'upblue',
-           'upgreen',
-           'upbrown']
+    train_label = ['backpack',
+                   'bag',
+                   'handbag',
+                   'boots',
+                   'gender',
+                   'hat',
+                   'shoes',
+                   'top',
+                   'downblack',
+                   'downwhite',
+                   'downred',
+                   'downgray',
+                   'downblue',
+                   'downgreen',
+                   'downbrown',
+                   'upblack',
+                   'upwhite',
+                   'upred',
+                   'uppurple',
+                   'upgray',
+                   'upblue',
+                   'upgreen',
+                   'upbrown']
+    
+    test_label=['boots',
+                'shoes',
+                'top',
+                'gender',
+                'hat',
+                'backpack',
+                'bag',
+                'handbag',
+                'downblack',
+                'downwhite',
+                'downred',
+                'downgray',
+                'downblue',
+                'downgreen',
+                'downbrown',
+                'upblack',
+                'upwhite',
+                'upred',
+                'upgray',
+                'upblue',
+                'upgreen',
+                'uppurple',
+                'upbrown']
     
     
     train_person_id = []
@@ -60,15 +84,36 @@ def import_DukeMTMCAttribute(dataset_dir):
                 if id not in locals()[group_name]:
                     locals()[group_name][id]=[]
                 locals()[group_name][id].append(f['duke_attribute'][0][0][test_train][0][0][attribute_id][0][person_id])
+    
+    for i in range(8):
+        train_label.insert(8,train_label[-1])
+        train_label.pop(-1)
+    
+    unified_train_atr = {}
+    for k,v in train_attribute.items():
+        temp_atr = list(v)
+        for i in range(8):
+            temp_atr.insert(8,temp_atr[-1])
+            temp_atr.pop(-1)
+        unified_train_atr[k] = temp_atr
+    
+    unified_test_atr = {}
+    for k,v in test_attribute.items():
+        temp_atr = [0]*len(train_label)
+        for i in range(len(train_label)):
+            temp_atr[i]=v[test_label.index(train_label[i])]
+        unified_test_atr[k] = temp_atr
     #two zero appear in train '0370' '0679'
-    zero_check=[]
-    for id in train_attribute:
-        if 0 in train_attribute[id]:
-            zero_check.append(id)
-    for i in range(len(zero_check)):
-        train_attribute[zero_check[i]] = [1 if x==0 else x for x in train_attribute[zero_check[i]]]
+    #zero_check=[]
+    #for id in train_attribute:
+    #    if 0 in train_attribute[id]:
+    #        zero_check.append(id)
+    #for i in range(len(zero_check)):
+    #    train_attribute[zero_check[i]] = [1 if x==0 else x for x in train_attribute[zero_check[i]]]
+    unified_train_atr['0370'][7]=1
+    unified_train_atr['0679'][7]=2
 
-    return train_attribute,test_attribute,label
+    return unified_train_atr,unified_test_atr,train_label
 
 def import_DukeMTMCAttribute_binary(dataset_dir):
 	train_duke_attr, test_duke_attr,label = import_DukeMTMCAttribute(dataset_dir)
